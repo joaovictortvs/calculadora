26f5b7229db64bfe580b4c2ffc4186b1e7e6e899
joaovictortvs <tavaressjoaoo09@gmail.com>
2025-01-15 22:08:52 -0300

    Adicionando verificações para os cálculos

--- src/components/Container.js
+++ src/components/Container.js
@@ -6,6 +6,8 @@ import Buttons from './Buttons'
 function Container(){
     const [display, setDisplay] = useState('0')
     const [result, setResult] = useState(null)
+    const [stateOperador, setStateOperador] = useState(false)
+
 
    function AddVisor(valor){
         if(display === '0'){
@@ -13,17 +15,28 @@ function Container(){
         } else{
             setDisplay(prevDisplay=> prevDisplay + valor)
         }
+        if(stateOperador){
+            setStateOperador(false)
+        }
    }
 
    function Calcular(operador){
+
         if(operador === "="){
             setResult(eval(display))
+            setStateOperador(false)
         } else if(operador === "CE"){
             setDisplay('0')
             setResult(null)
-        } else{
+            setStateOperador(false)
+        } else {
+            if(stateOperador){
+                alert('Não é possível adicionar dois operadores.')
+                return
+            }
+            setStateOperador(true)
             setDisplay(valor=> valor + operador)
-        }
+        } 
    }
 
     return(
